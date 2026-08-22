d3a1d137148830cb688853f58b691d0c1e331d75
JiahaoAlbus <jiahao15345932820@gmail.com>
2026-08-22 13:16:55 +0800

    wallet: allow online disposable QA builds

--- apps/wallet/scripts/build-disposable-android-qa-release.sh
+++ apps/wallet/scripts/build-disposable-android-qa-release.sh
@@ -62,7 +62,7 @@ fi
   YNX_ANDROID_KEYSTORE_PATH="$keystore" YNX_ANDROID_KEYSTORE_TYPE=PKCS12 \
   YNX_ANDROID_KEY_ALIAS="$alias_name" YNX_ANDROID_STORE_PASSWORD="$YNX_QA_STORE_PASSWORD" \
   YNX_ANDROID_KEY_PASSWORD="$YNX_QA_KEY_PASSWORD" \
-  ./gradlew "${gradle_network_args[@]}" --no-daemon --console=plain --max-workers=1 \
+  ./gradlew ${gradle_network_args[@]+"${gradle_network_args[@]}"} --no-daemon --console=plain --max-workers=1 \
     -Pkotlin.compiler.execution.strategy=in-process -PreactNativeArchitectures=arm64-v8a \
     -Pandroid.enableMinifyInReleaseBuilds=true -Pandroid.enableShrinkResourcesInReleaseBuilds=true \
     :app:assembleRelease
